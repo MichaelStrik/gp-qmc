1457b9c116a85d5a25bcecbfd115370b10ab6ace
Michael Strik <michael.strik@stud.uni-heidelberg.de>
2026-06-28 20:11:56 +0200

    plot layout changed

--- visualise.py
+++ visualise.py
@@ -3,8 +3,6 @@ plt.style.use('seaborn-v0_8-paper')
 import numpy as np
 import pickle
 
-from experiments import read_experiment_data
-
 
 def plot_experiment(experiment):
     # read out setup
@@ -32,22 +30,25 @@ def plot_experiment(experiment):
             ax2.loglog(Ndesign_list, conditioning_data[i,:], 'o-', label=f's={dim_list[i]}')
 
     # labels and co.
-    ax1.set(xlabel=f'N ({type_design_points} design points)', ylabel='error (shift-average L2)')
-    ax2.set(xlabel=f'N ({type_design_points} design points)', ylabel='condition number (average)')
+    fontsize = 11
+    ax1.set_xlabel(f'N ({type_design_points} design points)', fontsize=fontsize)
+    ax1.set_ylabel('L2 error (shift-average)', fontsize=fontsize)
+    ax2.set_xlabel(f'N ({type_design_points} design points)', fontsize=fontsize)
+    ax2.set_ylabel('condition number (shift-average)', fontsize=fontsize)
     ax1.legend()
     ax2.legend()
     ax1.grid()
     ax2.grid()
     ax1.grid(which="minor", color="0.9")
     ax2.grid(which="minor", color="0.9")
-    fig.suptitle(rf'GP/Kernel interpolant; $\gamma_j = 1/j^{{{weights_decay}}}$. Kernel: {experiment['kernel_class'].__name__}')
+    fig.suptitle(rf'Kernel interpolant; $\gamma_j = 1/j^{{{weights_decay}}}$. Kernel: {experiment['kernel_class'].__name__}', fontsize=fontsize+2)
+    fig.tight_layout()
     plt.show()
 
 
 if __name__ ==  '__main__':
     # define behavior as a command-line tool
     import argparse
-    import json
     parser = argparse.ArgumentParser()
     parser.add_argument("plot_fun", help="Experiment function to be run.")
     parser.add_argument("exp_obj", help="Path to pickle file containing experiment data.")
