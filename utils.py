013c918401233d9a68c4df8c2a8ddb0bc145cd70
Michael Strik <michael.strik@stud.uni-heidelberg.de>
2026-07-02 18:17:01 +0200

    feat: runtime logger

--- utils.py
+++ utils.py
@@ -1,3 +1,21 @@
+import time
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='logger.log', encoding='utf8', level=logging.INFO)
+
+# decorator
+def log_runtime(func):
+    def wrapper(*arg,**kw):
+        t1 = time.time()
+        res = func(*arg, **kw)
+        t2 = time.time()
+        runtime = t2-t1
+        logger.info(f"{func.__name__}: {runtime}")
+        return res
+    return wrapper
+
+
 def worker(input, output):
     for func, args in iter(input.get, 'STOP'):
         result = func(*args)
